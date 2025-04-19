87cfe9675dde8342a041b6342705c2baf76fc590
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-19 10:12:22 +0700

    Add files via upload

---
+++ javascript3.js
@@ -0,0 +1,17 @@
+const printSquare = (num) => {
+  for (let i = 1; i <= num; i++) {
+    let line = "";
+    for (let j = 1; j <= num; j++) {
+      line += j + " ";
+    }
+    console.log(line);
+  }
+};
+
+printSquare(3);
+printSquare(5);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const combine = [...arr1, ...arr2];
+console.log(combine);
